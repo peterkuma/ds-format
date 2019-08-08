b8cbe3467a85eb191086f782343084805ab7d8a7
Peter Kuma <peter.kuma@fastmail.com>
2019-08-08 19:10:08 +1200

    Improve setup.py meta

--- setup.py
+++ setup.py
@@ -10,13 +10,13 @@ setup(
     description='Python implementation of a dataset format DS for storing data along with metadata (in development)',
     author='Peter Kuma',
     author_email='peter.kuma@fastmail.com',
-    keywords=['dataset', 'hdf', 'json', 'netcdf'],
+    keywords=['dataset', 'netcdf', 'hdf', 'json', 'numpy']
     url='https://github.com/peterkuma/ds-python',
     platforms=['any'],
     install_requires=['netCDF4'],
     license='MIT',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
