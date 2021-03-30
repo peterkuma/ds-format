bdf253a2bc6064af8eb2f5e9757d438992f5b916
Peter Kuma <peter@peterkuma.net>
2021-03-31 00:56:37 +0200

    v1.1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ds-format',
-    version='1.0.1',
+    version='1.1.0',
     scripts=['bin/ds'],
     packages=find_packages(),
     description='Python implementation of a dataset format DS for storing data along with metadata',
