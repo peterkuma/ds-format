51f88c8259844063f1b4defe45adf1f7a4f460cf
Peter Kuma <peter@peterkuma.net>
2022-01-01 17:01:04 +0100

    setup.py: Change indentation to tabs

--- setup.py
+++ setup.py
@@ -3,29 +3,29 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='ds-format',
-    version='1.1.2',
-    scripts=['bin/ds'],
-    packages=find_packages(),
-    description='Python implementation of a dataset format DS for storing data along with metadata',
-    author='Peter Kuma',
-    author_email='peter@peterkuma.net',
-    keywords=['dataset', 'netcdf', 'hdf', 'json', 'numpy'],
-    url='https://github.com/peterkuma/ds-format',
-    platforms=['any'],
-    python_requires='>=3.0',
-    install_requires=[
+	name='ds-format',
+	version='1.1.2',
+	scripts=['bin/ds'],
+	packages=find_packages(),
+	description='Python implementation of a dataset format DS for storing data along with metadata',
+	author='Peter Kuma',
+	author_email='peter@peterkuma.net',
+	keywords=['dataset', 'netcdf', 'hdf', 'json', 'numpy'],
+	url='https://github.com/peterkuma/ds-format',
+	platforms=['any'],
+	python_requires='>=3.0',
+	install_requires=[
 		'netCDF4',
 		'cftime>=1.5.1',
 		'pst-format>=1.1.1',
 		'aquarius-time>=0.1.0'
 	],
-    license='MIT',
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3',
-    ],
+	license='MIT',
+	classifiers=[
+		'Development Status :: 5 - Production/Stable',
+		'Intended Audience :: Science/Research',
+		'License :: OSI Approved :: MIT License',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python :: 3',
+	],
 )
