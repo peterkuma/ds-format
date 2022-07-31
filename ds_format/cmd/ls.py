58b3ba843a38019c95d39ed54a53ab5b111fb30c
Peter Kuma <peter@peterkuma.net>
2022-07-31 17:48:18 +0100

    ds ls: Add a option and remove -L option

--- ds_format/cmd/ls.py
+++ ds_format/cmd/ls.py
@@ -16,45 +16,48 @@ def ls(*args, **opts):
 		*input*: "Input file."
 	}}
 	options: {{
-		`-l`: "Print a detailed list (dimensions and long_name)."
-		`-L`: "Print a more detailed list (dimensions and attributes)."
+		`-l`: "Print a detailed list (name and dimensions)."
+		"`-a` *attrs*": "Print given variable attributes. *attrs* can be a string or an array."
 	}}
+	desc: "Lines in the output are formatted as PST."
 	examples: {{
-"Print list variables in dataset.nc":
+"Print a list of variables in dataset.nc.":
 "$ ds dataset.nc
 temperature
 time"
 
-"Print detailed list of variables in dataset.nc":
+"Print a detailed list of variables in dataset.nc.":
 "$ ds -l dataset.nc
 temperature time: 3
 time time: 3"
+
+"Print a list of variables with an attribute `units`.":
+"$ ds dataset.nc a: units
+temperature celsius
+time s"
+
+"Print a list of variables with attributes `long_name` and `units`.":
+"$ ds dataset.nc a: { long_name units }
+temperature temperature celsius
+time time s"
 	}}
 	'''
 	input_ = args
 	for filename in input_:
 		d = ds.read(filename, [], full=True)
 		vars_ = sorted([x for x in d['.'].keys() if not x.startswith('.')])
-		if opts.get('l') or opts.get('L'):
-			for x in vars_:
+		for x in vars_:
+			y = [x]
+			if opts.get('l'):
 				dims = {dim: ds.get_meta(d, x)['.size'][i]
 					for i, dim in enumerate(ds.get_dims(d, x))}
+				if len(dims) > 0:
+					y += [dims]
+			if opts.get('a'):
 				attrs = ds.get_attrs(d, x)
-				y = [x]
-				if opts.get('l'):
-					if len(dims) > 0:
-						y += [dims]
-					if 'long_name' in attrs:
-						y += [attrs['long_name']]
-					s = pst.encode(y, encoder=misc.encoder)
-				elif opts.get('L'):
-					s = pst.encode([x, dims, attrs], encoder=misc.encoder)
-				print(s.decode('utf-8'))
-				#dims = d['.'][x]['.dims']
-				#size = d['.'][x]['.size']
-				#dims_s = ''
-				#for i, dim in enumerate(dims):
-				#	dims_s += '%s%s=%d' % (',' if i > 0 else '', dim, size[i])
-				#print('%s(%s)' % (x, dims_s))
-		else:
-			print('\n'.join(vars_))
+				if type(opts['a']) is list:
+					y += [attrs.get(a, None) for a in opts['a']]
+				elif type(opts['a']) is str:
+					y += [attrs.get(opts['a'], None)]
+			s = pst.encode(y, encoder=misc.encoder)
+			print(s.decode('utf-8'))
