f322c0d98cbaa1c70c855f82b3cc48994884a56f
Peter Kuma <peter@peterkuma.net>
2022-09-04 14:07:27 +0100

    Error handling command line options

--- ds_format/cmd/main.py
+++ ds_format/cmd/main.py
@@ -23,6 +23,9 @@ def main():
 	options: {{
 		`-F`: "Interpret variable, dimension and attribute names as fixed strings, not glob patterns."
 		`--help`: "Show this help message or help for a command."
+		`-m`: "Moderate error handling mode. Report a warning on missing variables, dimensions and attributes."
+		`-s`: "Strict error handling mode. Handle missing variables, dimensions and attributes as errors. Overrides DS_MODE environment variable."
+		`-t`: "Soft error handling mode. Ignore missing variables, dimensions and attributes. Overrides DS_MODE environment variable."
 		`-v`: "Be verbose. Print more detailed information and error messages."
 		`--version`: "Print the version number and exit."
 	}}
@@ -86,6 +89,12 @@ def main():
 	if opts.get('version'):
 		print(ds.__version__)
 		sys.exit(0)
+	if opts.get('t'):
+		ds.mode = 'soft'
+	if opts.get('m'):
+		ds.mode = 'moderate'
+	if opts.get('s'):
+		ds.mode = 'strict'
 	if len(args) == 0:
 		sys.stderr.write('Usage: ds [CMD] [OPTIONS]\n')
 		sys.stderr.write('       ds --help\n')
