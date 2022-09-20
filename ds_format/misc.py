a30b31935f2f65d1dbef962ba2254fb92bf38986
Peter Kuma <peter@peterkuma.net>
2022-09-20 14:42:00 +0100

    type_to_dtype: Fix dtype output

--- ds_format/misc.py
+++ ds_format/misc.py
@@ -12,19 +12,19 @@ KIND_TO_TYPE = {
 }
 
 TYPE_TO_DTYPE = {
-	'float32': np.float32,
-	'float64': np.float64,
-	'int8': np.int8,
-	'int16': np.int16,
-	'int32': np.int32,
-	'int64': np.int64,
-	'uint8': np.uint8,
-	'uint16': np.uint16,
-	'uint32': np.uint32,
-	'uint64': np.uint64,
-	'bool': np.bool,
-	'str': np.str,
-	'unicode': np.unicode,
+	'float32': np.dtype('float32'),
+	'float64': np.dtype('float64'),
+	'int8': np.dtype('int8'),
+	'int16': np.dtype('int16'),
+	'int32': np.dtype('int32'),
+	'int64': np.dtype('int64'),
+	'uint8': np.dtype('uint8'),
+	'uint16': np.dtype('uint16'),
+	'uint32': np.dtype('uint32'),
+	'uint64': np.dtype('uint64'),
+	'bool': np.dtype('bool'),
+	'str': np.dtype('str'),
+	'unicode': np.dtype('unicode'),
 }
 
 def sel_slice(sel, dims):
