99295f4bc1ad5d0369f59dcb9b7cb6e769393464
Peter Kuma <peter@peterkuma.net>
2023-05-25 12:14:51 +0100

    csv: Add support for tab/tsv format

--- ds_format/drivers/csv.py
+++ ds_format/drivers/csv.py
@@ -3,8 +3,8 @@ import numpy as np
 import ds_format as ds
 from ds_format import misc
 
-READ_EXT = ['csv']
-WRITE_EXT = ['csv']
+READ_EXT = ['csv', 'tsv', 'tab']
+WRITE_EXT = ['csv', 'tsv', 'tab']
 
 def convert(x):
 	for f in [int, float]:
@@ -12,12 +12,14 @@ def convert(x):
 		except ValueError: pass
 	return x
 
-def read(filename, variables=None, sel=None, full=False, jd=False):
+def read(filename, variables=None, sel=None, full=False, jd=False, opts={}):
 	header = None
 	ncols = 0
 	x = []
+	sep = opts.get('sep')
+	if sep is None: sep = '\t' if filename.endswith(('tsv', 'tab')) else ','
 	with open(filename) as f:
-		reader = csv.reader(f)
+		reader = csv.reader(f, delimiter=sep)
 		for row in reader:
 			if header is None:
 				header = row
@@ -46,10 +48,12 @@ def read(filename, variables=None, sel=None, full=False, jd=False):
 		ds.select(d, sel)
 	return d
 
-def write(filename, d):
+def write(filename, d, opts={}):
 	ds.validate(d)
 	header = []
 	x = []
+	sep = opts.get('sep')
+	if sep is None: sep = '\t' if filename.endswith(('tsv', 'tab')) else ','
 	for var in ds.vars(d):
 		data = ds.var(d, var)
 		if data is not None and data.ndim > 1:
@@ -65,7 +69,7 @@ def write(filename, d):
 	else:
 		nrows = 0
 	with open(filename, 'w') as f:
-		writer = csv.writer(f)
+		writer = csv.writer(f, delimiter=sep)
 		if ncols > 0:
 			writer.writerow(header)
 		for j in range(nrows):
